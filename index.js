446fd275a755ee3906af71ee817d698fd32bfe28
cactusflips2020 <steven.sadecki@gmail.com>
2025-02-21 16:37:29 -0600

    pluh

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
 import { ChemicalServer } from "chemicaljs";
 import express from "express";
-import path from "path";
 
-const [app, listen] = new ChemicalServer({
-    uv: true,
-    scramjet: false,
-    rammerhead: false,
-});
+const [app, listen] = new ChemicalServer();
+const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 8080;
-const __dirname = process.cwd();
-
-app.use(express.static("static"));
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "files"));
-
-app.get("/", (req, res) => {
-    res.render("index");
-}) 
-
-app.get("/~", (req, res) => {
-    res.render("proxy");
-});
+app.use(express.static("public", {
+    index: "index.html",
+    extensions: ["html"]
+}));
 
 app.serveChemical();
 
 listen(port, () => {
-    console.log(`Mewing Academy is listening on port ${port}!`);
-    console.log(`\t1. http://localhost:${port}`);
+    console.log(`Mewing Academy listening on port ${port}!`);
 });
